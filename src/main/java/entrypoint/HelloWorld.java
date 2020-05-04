10701f7f742b2520758f3b3704fafd42f22d375c
Andrea Graziani <andrea.graziani93@outlook.it>
2020-05-04 11:46:17 +0200

    #3 Fix smells

---
+++ src/main/java/entrypoint/HelloWorld.java
@@ -0,0 +1,14 @@
+package entrypoint;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        Logger logger = Logger.getLogger(HelloWorld.class.getName());
+
+        logger.log(Level.INFO, "Program started...");
+    }
+}
